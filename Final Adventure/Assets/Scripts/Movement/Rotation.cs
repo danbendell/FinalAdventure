3d799582b92d111b1612a8f63005f753f8e1feaf
danbendell <danbendell@gmail.com>
2016-03-31 17:03:59 +0100

    Start of rotating character

---
+++ Final Adventure/Assets/Scripts/Movement/Rotation.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Movement;
+
+public class Rotation : MonoBehaviour {
+
+
+    private Vector3 _left = new Vector3(0f, 0f, -1f);
+    private Vector3 _right = new Vector3(0f, 0f, 1f);
+    private Vector3 _top = new Vector3(-1f, 0f, 0f);
+    private Vector3 _bottom = new Vector3(1f, 0f, 0f);
+
+    private CharacterHolder _characterHolder;
+    private Vector2 _pointer;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CharactersController CC = GameObject.Find("Characters").GetComponent<CharactersController>();
+        _characterHolder = CC.CurrentCharacterHolder;
+	    _pointer = GameObject.Find("Floor").GetComponent<FloorHighlight>().PointerPosition;
+
+        float differenceInX = CalculatePositiveDifference(_characterHolder.Character.XyPosition().x, _pointer.x);
+        float differnceInY = CalculatePositiveDifference(_characterHolder.Character.XyPosition().y, _pointer.y);
+
+	    float direction = differenceInX - differnceInY;
+	    if (direction > 0) RotateLeftOrRight(_characterHolder.Character.XyPosition().x, _pointer.x);
+	    if (direction < 0) RotateTopOrBottom(_characterHolder.Character.XyPosition().y, _pointer.y);
+
+	}
+
+    private void RotateLeftOrRight(float valueOne, float valueTwo)
+    {
+        if ((valueOne - valueTwo) > 0) _characterHolder.transform.forward = _left;
+        if ((valueOne - valueTwo) < 0) _characterHolder.transform.forward = _right;
+    }
+
+    private void RotateTopOrBottom(float valueOne, float valueTwo)
+    {
+        if ((valueOne - valueTwo) > 0) _characterHolder.transform.forward = _bottom;
+        if ((valueOne - valueTwo) < 0) _characterHolder.transform.forward = _top;
+    }
+
+    private float CalculatePositiveDifference(float valueOne, float valueTwo)
+    {
+        if (valueOne > valueTwo)
+        {
+            return valueOne - valueTwo;
+        }
+        else
+        {
+            return valueTwo - valueOne;
+        }
+    }
+}
---
+++ Final Adventure/Assets/Scripts/Movement/RotationNodes.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationNodes : MonoBehaviour
+{
+    public Material Normal;
+    public Material Highlight;
+
+    private Vector3 _left = new Vector3(0f, 0f, -1f); 
+    private Vector3 _right = new Vector3(0f, 0f, 1f); 
+    private Vector3 _top = new Vector3(-1f, 0f, 0f); 
+    private Vector3 _bottom = new Vector3(1f, 0f, 0f); 
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CharactersController CC = GameObject.Find("Characters").GetComponent<CharactersController>();
+        CharacterHolder CH = CC.CurrentCharacterHolder;
+        
+        if (Input.GetKeyDown(KeyCode.W))
+	    {
+	        FindChild("Top");
+            //CH.transform.transform.forward = _top;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            FindChild("Bottom");
+            //CH.transform.transform.forward = _bottom;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            FindChild("Left");
+           // CH.transform.transform.forward = _left;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            FindChild("Right");
+            //CH.transform.transform.forward = _right;
+        }
+
+
+    }
+
+    private void FindChild(string value)
+    {
+        HighlightNode(value);
+       
+    }
+
+    private void HighlightNode(string value)
+    {
+        Renderer[] renderers = transform.GetComponentsInChildren<Renderer>();
+        for (var i = 0; i < renderers.Length; i++)
+        {
+            if (renderers[i].name != value)
+            {
+                renderers[i].material = Normal;
+            }
+            else
+            {
+                renderers[i].material = Highlight;
+                Light light = transform.GetComponentInChildren<Light>();
+                light.transform.position = renderers[i].transform.position;
+            }
+        }
+    }
+}
