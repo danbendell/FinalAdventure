8c6df4e04baf66ac6273efb75e945aa19eb28f65
danbendell <danbendell@gmail.com>
2016-03-31 15:32:43 +0100

    Text Color Fix

--- Final Adventure/Assets/Scripts/Interactions/DamageText.cs
+++ Final Adventure/Assets/Scripts/Interactions/DamageText.cs
@@ -4,20 +4,18 @@ using UnityEngine.UI;
 
 public class DamageText : MonoBehaviour
 {
-    private Color _red = new Color(0.56f, 0.1f, 0.12f, 0.75f);
-    private Color _green = new Color(0.15f, 1f, 0.1f, 0.75f);
+    private Color _red = new Color(143.0f/255.0f, 26.0f/255.0f, 31.0f/255.0f, 255.0f/255.0f);
+    private readonly Color _green = new Color(51.0f/255.0f, 181.0f/255.0f, 56.0f/255.0f, 255.0f/255.0f);
     public GameObject GameObject;
     private Text _text;
     private float _yPosition;
-    private int _size;
     private float _alpha;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    _text = GameObject.GetComponent<Text>();
-	    _yPosition = 0f;
-	    _size = 30;
+        _text = GameObject.GetComponent<Text>();
+        _yPosition = 0f;
         _text.color = new Color(_text.color.a, _text.color.g, _text.color.b, 0.0f); 
 	    //_alpha = 0.75f;
 	}
@@ -29,7 +27,7 @@ public class DamageText : MonoBehaviour
 	        AnimateDamage(5);
 	    }
         GameObject.GetComponent<RectTransform>().anchoredPosition3D = Vector3.Lerp(GameObject.GetComponent<RectTransform>().anchoredPosition3D, new Vector3(GameObject.GetComponent<RectTransform>().anchoredPosition3D.x, _yPosition, GameObject.GetComponent<RectTransform>().anchoredPosition3D.z), 3 * Time.deltaTime);
-        _text.color = new Color(_text.color.a, _text.color.g, _text.color.b, Mathf.Lerp(_text.color.a, _alpha, 5 * Time.deltaTime));
+        _text.color = new Color(_text.color.r, _text.color.g, _text.color.b, Mathf.Lerp(_text.color.a, _alpha, 5 * Time.deltaTime));
     }
 
     public void AnimateDamage(int value)
