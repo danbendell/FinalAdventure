b00c14739be2bac79e908e65cb943050ab29754c
danbendell <danbendell@gmail.com>
2016-05-17 14:39:22 +0100

    Random work, report, bug fixes, assets

--- FinalAdventureWeb/routes.js
+++ FinalAdventureWeb/routes.js
@@ -2,6 +2,11 @@ var sqlAzure = require('msnodesqlv8');
 var sql = require('mssql');
 module.exports = function(router)
 {
+
+    router.get('/', function (req, res) {
+        res.status(200).send("WORKING");
+    });
+
     router.post('/api/Turn', function (req, res)
     {
         var Job =  "'" + req.body.Job + "'";
@@ -17,40 +22,60 @@ module.exports = function(router)
             req.body.Move + ', ' +
             Action + ')';
 
-        var config = {
-            user: 'dan',
-            password: 'password',
-            server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
-            database: 'PRCO304-FinalAdventure',
-            port: 1433,
+        //Azure
+        var connectionString = 'Driver={SQL Server Native Client 11.0};Server=tcp:prco304-server.database.windows.net,1433;Database=PRCO304DB;Uid=danbendell@prco304-server;Pwd=HVB37mdh;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;';
 
-            options: {
-                encrypt: true // Use this if you're on Windows Azure
-            }
-        };
-        console.log(sqlQuery);
-        sql.connect(config).then(function() {
-
-            var connection = new sql.Connection(config, function(err) {
-                if(err) {
-                    console.log("ERROR: " + err);
-                }
-                // Query
-                var request = new sql.Request(connection);
-                request.query(sqlQuery, function(err) {
-                    //... error checks
-                    if(err) {
-                        console.log(err);
-                    } else{
-                        res.status(200).send();
-                    }
-                });
-            });
-        }).catch(function(err) {
+        //Live Azure
+        sqlAzure.open(connectionString, function(err, conn) {
             if(err) {
-                console.log("CONNECTION ERROR: " + err);
+                console.log(err);
             }
+            console.log("HERE");
+            conn.query(sqlQuery, function(err, record) {
+                if(err) {
+                    res.status(0).send("SELECT * FROM User - Failed");
+                    console.log(err);
+                }
+                console.log(record);
+                res.status(200).send(record);
+            });
         });
+
+        //Local
+        //var config = {
+        //    user: 'dan',
+        //    password: 'password',
+        //    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+        //    database: 'PRCO304-FinalAdventure',
+        //    port: 1433,
+        //
+        //    options: {
+        //        encrypt: true // Use this if you're on Windows Azure
+        //    }
+        //};
+        //console.log(sqlQuery);
+        //sql.connect(config).then(function() {
+        //
+        //    var connection = new sql.Connection(config, function(err) {
+        //        if(err) {
+        //            console.log("ERROR: " + err);
+        //        }
+        //        // Query
+        //        var request = new sql.Request(connection);
+        //        request.query(sqlQuery, function(err) {
+        //            //... error checks
+        //            if(err) {
+        //                console.log(err);
+        //            } else{
+        //                res.status(200).send();
+        //            }
+        //        });
+        //    });
+        //}).catch(function(err) {
+        //    if(err) {
+        //        console.log("CONNECTION ERROR: " + err);
+        //    }
+        //});
     });
 
     router.get('/api/Turn', function (req, res)
@@ -89,58 +114,58 @@ module.exports = function(router)
         };
 
         //var config = {
-        //    user: 'daniel.bendell',
+        //    user: 'danbendell',
         //    password: 'HVB37mdh',
-        //    server: 'whatsup.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-        //    database: 'MoblieDb',
+        //    server: 'prco304-server.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+        //    database: 'PRCO304DB',
         //
         //    options: {
         //        encrypt: true // Use this if you're on Windows Azure
         //    }
         //};
-        var connectionString = 'Driver={SQL Server Native Client 11.0};Server=whatsup.database.windows.net,1433;Database=MobileDb;Uid=daniel.bendell@whatsup;Pwd={HVB37mdh};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;';
+        var connectionString = 'Driver={SQL Server Native Client 11.0};Server=tcp:prco304-server.database.windows.net,1433;Database=PRCO304DB;Uid=danbendell@prco304-server;Pwd=HVB37mdh;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;';
 
         //Live Azure
-        //sqlAzure.open(connectionString, function(err, conn) {
-        //   if(err) {
-        //       console.log(err);
-        //   }
-        //    console.log("HERE");
-        //    conn.query("SELECT * FROM Users", function(err, record) {
-        //        if(err) {
-        //            res.status(0).send("SELECT * FROM User - Failed");
-        //            console.log(err);
-        //        }
-        //        console.log(record);
-        //        res.status(200).send(record);
-        //    });
-        //});
+        sqlAzure.open(connectionString, function(err, conn) {
+           if(err) {
+               console.log(err);
+           }
+            console.log("HERE");
+            conn.query("SELECT * FROM Turns", function(err, record) {
+                if(err) {
+                    res.status(0).send("SELECT * FROM User - Failed");
+                    console.log(err);
+                }
+                console.log(record);
+                res.status(200).send(record);
+            });
+        });
 
         //Local DB
-        sql.connect(config).then(function() {
-
-            var connection = new sql.Connection(config, function(err) {
-                if(err) {
-                    console.log("ERROR: " + err);
-                }
-                // Query
-                var request = new sql.Request(connection);
-                console.log(sqlQuery);
-                request.query(sqlQuery, function(err, recordset) {
-                    //... error checks
-                    if(err) {
-                        console.log(err);
-                    }
-                    //console.dir(recordset);
-                    res.status(200).send(recordset);
-
-                });
-            });
-        }).catch(function(err) {
-            if(err) {
-                console.log("CONNECTION ERROR: " + err);
-            }
-        });
+        //sql.connect(config).then(function() {
+        //
+        //    var connection = new sql.Connection(config, function(err) {
+        //        if(err) {
+        //            console.log("ERROR: " + err);
+        //        }
+        //        // Query
+        //        var request = new sql.Request(connection);
+        //        console.log(sqlQuery);
+        //        request.query(sqlQuery, function(err, recordset) {
+        //            //... error checks
+        //            if(err) {
+        //                console.log(err);
+        //            }
+        //            //console.dir(recordset);
+        //            res.status(200).send(recordset);
+        //
+        //        });
+        //    });
+        //}).catch(function(err) {
+        //    if(err) {
+        //        console.log("CONNECTION ERROR: " + err);
+        //    }
+        //});
 
 
     });
