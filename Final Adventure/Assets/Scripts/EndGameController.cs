504663f720281c5e07900b0200f95785d5111819
danbendell <danbendell@gmail.com>
2016-05-17 14:33:32 +0100

    Sky box and bug fixes

---
+++ Final Adventure/Assets/Scripts/EndGameController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGameController : MonoBehaviour
+{
+
+    public GameObject EndGame;
+    public GameObject Victory;
+    public GameObject Defeat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (EndGame.activeSelf == false) return;
+	    if (Input.GetKeyDown(KeyCode.Return))
+	    {
+            Destroy(GameObject.Find("Characters"));
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+	}
+
+    public IEnumerator ShowVictory()
+    {
+        yield return new WaitForSeconds(1f);
+        EndGame.SetActive(true);
+        Victory.SetActive(true);
+    }
+
+    public IEnumerator ShowDefeat()
+    {
+        yield return new WaitForSeconds(1f);
+        EndGame.SetActive(true);
+        Defeat.SetActive(true);
+    }
+}
